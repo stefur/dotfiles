34121d6786be85e9bce9a699721e431ec714cc60
stefur <stefan@furne.net>
2022-07-21 17:53:01 +0200

    Fixed idle, battery and fonts

--- local/bin/idle.sh
+++ local/bin/idle.sh
@@ -1,10 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-if pgrep "swayidle" > /dev/null
-then
-    pkill swayidle
-    pkill -RTMIN+7 waybar
-else
-    swayidle -w	timeout 300 "lock.sh" timeout 600 "wlopm --off '*'" timeout 900 "loginctl suspend" resume "wlopm --on '*'" before-sleep "lock.sh" &
-    pkill -RTMIN+7 waybar
+status=$(pgrep -a swayidle)
+
+if [ $# -eq 0 ]; then
+ echo "Arguments required. Exiting."
+ exit 1
 fi
+
+case "${1}" in
+    toggle)
+        if [[ $status =~ "timeout" ]]; then
+            pkill swayidle
+            swayidle -w	before-sleep "lock.sh" &
+            pkill -RTMIN+7 waybar
+        else
+            pkill swayidle
+            swayidle -w	timeout 300 "lock.sh" timeout 600 "wlopm --off '*'" timeout 900 "loginctl suspend" resume "wlopm --on '*'" before-sleep "lock.sh" &
+            pkill -RTMIN+7 waybar
+        fi
+    ;;
+    status)
+        if [[ $status =~ "timeout" ]]; then
+            echo "﯈"
+        else
+            echo ""
+        fi
+    ;;
+    startup)
+        swayidle -w	timeout 300 "lock.sh" timeout 600 "wlopm --off '*'" timeout 900 "loginctl suspend" resume "wlopm --on '*'" before-sleep "lock.sh" &
+    ;;
+esac
