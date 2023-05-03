cc76f29d85df96ca500cea0381f5f767b467419a
stefur <stefan@furne.net>
2023-05-03 19:34:32 +0200

    Updated nerdfonts

--- local/bin/fuzzel.sh
+++ local/bin/fuzzel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # <&0 redirects stdin (fd 0) to fuzzel's stdin
 # This is leveraged by both pass.sh, emojis.sh & mount.sh
-fuzzel -f furamononerdfont:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b 16161DFF -t D9E0EEFF -m 9CABCAFF -s 54546DFF -S 16161DFF -B 2 -r 0 -C 9CABCAFF -I $@ <&0
+fuzzel -f firamononerdfont:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b 16161DFF -t D9E0EEFF -m 9CABCAFF -s 54546DFF -S 16161DFF -B 2 -r 0 -C 9CABCAFF -I $@ <&0
