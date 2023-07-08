b9cb471f27d0926766bcdf81c91779c18b2fb811
stefur <stefan@furne.net>
2023-07-08 09:45:42 +0200

    More color tweaks

--- local/bin/fuzzel.sh
+++ local/bin/fuzzel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # <&0 redirects stdin (fd 0) to fuzzel's stdin
 # This is leveraged by both pass.sh, emojis.sh & mount.sh
-fuzzel -f firamononerdfont:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b 16161DFF -t D9E0EEFF -m 9CABCAFF -s 54546DFF -S 16161DFF -B 2 -r 0 -C 9CABCAFF -I $@ <&0
+fuzzel -f firamononerdfont:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b C2C9AFFF -t 1d1d16FF -s B6AFC9FF -S 1d1d16FF -B 0 -r 10 -I $@ <&0
