a763f805d46c4e5ecb3deb6f0f71b3506e39f535
Stefan Furne <stefan@furne.net>
2024-10-30 22:04:26 +0100

    Fonts and gestures

--- local/bin/fuzzel.sh
+++ local/bin/fuzzel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # <&0 redirects stdin (fd 0) to fuzzel's stdin
 # This is leveraged by both pass.sh, emojis.sh & mount.sh
-fuzzel -f firamononerdfont:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b b6afc9ff -t 1d1d16ff -s d9e0eeff -S 1d1d16ff -B 0 -r 10 -I $@ <&0
+fuzzel -f firacoderetina:size=12,notocoloremoji:size=12 --dpi-aware=no -T foot -w 42 -b b6afc9ff -t 1d1d16ff -s d9e0eeff -S 1d1d16ff -B 0 -r 10 -I $@ <&0
